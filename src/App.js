faa59e459c763fccfc8d2c3f4795ef63dca55331
Damian <damianklepacki94@gmail.com>
2018-12-14 13:24:50 +0000

    Improved destroy function

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import contract from './blockchain/localInstanceOfContract.js';
 import web3 from './blockchain/web3.js'
 import './App.css';
 
+const withdrawAddress ='0xb06029664056f44437F4E82cFB2D6B153513F0B8'
 class App extends Component {
   state ={
     createAmount:"",
     destroyAmount:"",
     to:"",
-    supplay:""
+    supplay:"",
+    balance:""
   }
   async componentDidMount(){
   const supplay = await contract.methods.totalSupply().call();
-  this.setState({supplay})
+  const balance = await contract.methods.balanceOf(withdrawAddress).call();
+  this.setState({supplay,balance})
   }
 
   mintNewToken = async event =>{
@@ -27,10 +30,10 @@ class App extends Component {
   destroyTokens = async event =>{
     event.preventDefault();
     const accounts = await web3.eth.getAccounts();
-    await contract.methods.destroyTokens('0xb06029664056f44437F4E82cFB2D6B153513F0B8',this.state.destroyAmount).send({
+    await contract.methods.destroyTokens(withdrawAddress,this.state.balance).send({
       from: accounts[0]
     });
-
+    this.componentDidMount()
   };
 
 
@@ -39,7 +42,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <h2>GBP crypto coin supplay is: {this.state.supplay}</h2>
-          <p>GBP special withdraw address: 0xb06029664056f44437F4E82cFB2D6B153513F0B8</p>
+          <p>GBP special withdraw address: {withdrawAddress}</p>
          <form onSubmit ={this.mintNewToken}>
         <label> Mint new GBP crypto coins</label>
         <div>
@@ -55,19 +58,10 @@ class App extends Component {
         /> 
         </div>
         <button> Mint </button>
-         </form>
-
-         <form onSubmit ={this.destroyTokens}>
-        <label> Destroy GBP coins</label>
-        <div>
-        <input
-        value= {this.state.destroyAmount}
-        placeholder ="Amount to destroy"
-        onChange ={event => this.setState({destroyAmount: event.target.value})}
-        />
-        </div>
-        <button> Destroy </button>
-         </form>
+        </form>
+        <p>Destroy GBP crypto coins</p>
+        <button onClick={this.destroyTokens}> Destroy </button>
+  
 
 
 
